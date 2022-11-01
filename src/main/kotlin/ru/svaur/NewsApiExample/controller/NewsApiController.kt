939834fc8d5488b0b39790d57313b637296cba99
Alex <Sviridov.al.ur@gmail.com>
2022-11-01 20:13:50 +0300

    add validation for parameters

--- src/main/kotlin/ru/svaur/NewsApiExample/controller/NewsApiController.kt
+++ src/main/kotlin/ru/svaur/NewsApiExample/controller/NewsApiController.kt
@@ -5,7 +5,13 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.LoggerFactory
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.http.HttpStatus
+import org.springframework.validation.FieldError
+import org.springframework.validation.ObjectError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import ru.svaur.NewsApiExample.ArticlesDto
 import ru.svaur.NewsApiExample.dto.ParamsSourcesEnum
@@ -14,6 +20,11 @@ import ru.svaur.NewsApiExample.dto.SourcesDto
 import ru.svaur.NewsApiExample.feign.DownloadFeignClient
 import ru.svaur.NewsApiExample.feign.NewsApiFeignClient
 import java.net.URI
+import java.util.*
+import java.util.function.Consumer
+import javax.validation.Valid
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 
 /**
@@ -22,6 +33,9 @@ import java.net.URI
  * @property newsApiService
  * @constructor Create empty News api controller
  */
+//constant for URL validation
+const val URL_REGEXP = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"
+
 @RestController
 @Tag(name = "News Api Controller", description = "REST endpoints for NewsApiExample")
 class NewsApiController(
@@ -31,21 +45,19 @@ class NewsApiController(
     private val log = LoggerFactory.getLogger(NewsApiController::class.java)
     private var apiKey = System.getenv("API_KEY")
 
-
     @Cacheable(value = ["SourcesDto"], key = "#country")
     @GetMapping("/v1/getTopHeadlinesInCountry")
     @Operation(method = "getTopHeadlinesInCountry", summary = "get top headlines by country", operationId = "getTopHeadlinesInCountry")
-    fun getTopHeadlinesInCountry(country: String): ArticlesDto {
+    fun getTopHeadlinesInCountry(@Size(max = 2) country: String): ArticlesDto {
         val paramsMap = mapOf<String, String>(
                 ParamsTopArticlesEnum.APIKEY.param to apiKey,
                 ParamsTopArticlesEnum.COUNTRY.param to country
         )
         log.debug("request params:${paramsMap}}")
-        log.info("request params:${paramsMap}}")
         try {
             return newsApiFeignClient.getTopHeadlinersByCountry(paramsMap)
         } catch (e: FeignException) {
-            log.error("(NewsApiController/getTopHeadlinesInCountry) INTERNAL_SERVER_ERROR RuntimeException", e)
+            log.error("INTERNAL_SERVER_ERROR RuntimeException", e)
         }
 
         return ArticlesDto(0, "", 0, null)
@@ -71,9 +83,23 @@ class NewsApiController(
 
     @GetMapping("/v1/fileDownload")
     @Operation(method = "fileDownload", summary = "download file by URL", operationId = "fileDownload")
-    fun fileDownload(url: String): ByteArray {
+    fun fileDownload(@Valid @Pattern(regexp = URL_REGEXP) url: String): ByteArray {
         log.debug("downloading file ", url)
+        //todo:antivirus check up
         val response: ByteArray = downloadFeignClient.downloadFile(URI.create(url))
         return response
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(
+            ex: MethodArgumentNotValidException): Map<String, String?>? {
+        val errors: MutableMap<String, String?> = HashMap()
+        ex.bindingResult.allErrors.forEach(Consumer { error: ObjectError ->
+            val fieldName = (error as FieldError).field
+            val errorMessage = error.getDefaultMessage()
+            errors[fieldName] = errorMessage
+        })
+        return errors
     }
 }
